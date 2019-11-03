4c6c7f4c43e6289495c6244c35b7f752d0952169
Duy <pngocduy@tma.com.vn>
2019-11-03 09:22:20 +0700

    F1-0

--- GitTest.Net/GitTest.Net/Feature1.cs
+++ GitTest.Net/GitTest.Net/Feature1.cs
@@ -1,9 +1,15 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace GitTest.Net
 {
+    class Feature0
+    {
+        public int MyProperty { get; set; }
+        public int MyProperty1 { get; set; }
+        public int MyProperty2 { get; set; }
+    }
     class Feature1
     {
         public int MyProperty { get; set; }
